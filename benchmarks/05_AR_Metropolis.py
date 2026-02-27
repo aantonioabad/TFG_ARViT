43da3b746fd48aea3ced9cb1e6edfbc01f955de4
aantonioabad <842579@unizar.es>
2026-02-27 17:09:18 +0100

    05

--- benchmarks/05_AR_Metropolis.py
+++ benchmarks/05_AR_Metropolis.py
@@ -1,57 +1,65 @@
 import os
 import sys
+import time
+import jax
+import netket as nk
+import optax
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
 sys.path.append(parent_dir)
+
 os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
 
-import netket as nk
-import optax
-import time
 from physics.hamiltonian import get_Hamiltonian
-from models.vit import ARSpinViT_Manual
 
 def run_ar_metropolis():
-    print(">>> BENCHMARK 05: ARNN SOLO ANSATZ (Metropolis)")
-    print(">>> Objetivo: Demostrar que sin Sampleo Directo, es lento.")
+    print(">>> BENCHMARK 05: ARNN + METROPOLIS")
     print("---------------------------------------------------------")
     
-    # 1. Sistema
     N = 10
     hi = nk.hilbert.Spin(s=0.5, N=N)
     H = get_Hamiltonian(N, J=1.0, alpha=3.0, hilbert=hi)
 
-    # 2. Modelo (El Autoregresivo)
-    
-    model = ARSpinViT_Manual(
-        hilbert=hi, 
-        embedding_d=8,   
-        n_blocks=2, 
-        n_heads=2,          
-        n_ffn_layers=1     
+    try:
+        with open("benchmark_exact.txt", "r") as f:
+            E_exact = float(f.read())
+    except:
+        E_exact = None
+
+    model = nk.models.ARNNDense(
+        hilbert=hi,
+        layers=3,
+        features=16
     )
 
-    # 3. Sampler 
-    
     sampler = nk.sampler.MetropolisLocal(hi)
-
-    # 4. Estado Variacional
     vstate = nk.vqs.MCState(sampler, model, n_samples=2048, seed=42)
-
-    # 5. Optimizador
+    
     optimizer = optax.adam(learning_rate=0.001)
     gs = nk.driver.VMC_SR(H, optimizer, variational_state=vstate, diag_shift=0.1)
 
-    # 6. Ejecutar
+    print("Precalentando y compilando con JAX...")
+    gs.run(n_iter=1, show_progress=False)
+    jax.block_until_ready(vstate.variables)
+
+    print("Iniciando benchmark cronometrado...")
     start_time = time.time()
+    
     log = nk.logging.JsonLog("resultado_benchmark_05", save_params=False)
-    gs.run(n_iter=300, out=log, show_progress=True)
+    gs.run(n_iter=1500, out=log, show_progress=True)
+    
+    jax.block_until_ready(vstate.variables)
     end_time = time.time()
     
-    print(f"\nResultados AR + Metropolis:")
-    print(f"Energía: {log['Energy'].Mean[-1]:.6f}")
-    print(f"Tiempo : {end_time - start_time:.2f} s")
+    E_final = log["Energy"].Mean[-1]
+    
+    print("\n>>> RESULTADOS 05_AR_METROPOLIS:")
+    print(f"Energia AR-Metropolis : {E_final:.6f}")
+    if E_exact:
+        print(f"Energia Exacta        : {E_exact:.6f}")
+        print(f"Error                 : {abs((E_final - E_exact)/E_exact):.2%}")
+    print(f"Tiempo de ejecucion puro: {end_time - start_time:.2f} s")
 
 if __name__ == "__main__":
     run_ar_metropolis()
