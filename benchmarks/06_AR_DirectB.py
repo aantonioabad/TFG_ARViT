b992258eeeab01df9e955e8adeec17c198493996
aantonioabad <842579@unizar.es>
2026-03-03 10:23:41 +0100

    more

--- benchmarks/06_AR_DirectB.py
+++ benchmarks/06_AR_DirectB.py
@@ -54,7 +54,7 @@ def run_ar_direct_vit():
     start_time = time.time()
     
     log = nk.logging.JsonLog("resultado_benchmark_06B", save_params=False)
-    gs.run(n_iter=500, out=log, show_progress=True)
+    gs.run(n_iter=1000, out=log, show_progress=True)
     
     jax.block_until_ready(vstate.variables)
     end_time = time.time()
