e55bfd8c28fd63525210142d9423d2886522257c
aantonioabad <842579@unizar.es>
2026-04-08 13:49:48 +0200

    ising

--- benchmarks/07_ising_baselines_exactos.py
+++ benchmarks/07_ising_baselines_exactos.py
@@ -7,14 +7,14 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
 sys.path.append(parent_dir)
 
-from physics.ising_netket import get_native_Ising
+from physics.ising_netket import get_Ising
 
 def calcular_energia_exacta(N, J, dimensions, phase_name):
     print(f"\n{'-'*50}")
     print(f">>> MODELO ISING {dimensions}D | FASE: {phase_name} | N={N} | J={J}")
     
     # 1. Obtenemos el Hamiltoniano básico (Vecinos más cercanos)
-    hi, H = get_native_Ising(N=N, J=J, h_x=1.0, dimensions=dimensions)
+    hi, H = get_Ising(N=N, J=J, h_x=1.0, dimensions=dimensions)
     
     # 2. Convertimos a matriz dispersa (sparse) para que quepa en la memoria
     H_sparse = H.to_sparse()
@@ -37,4 +37,3 @@ if __name__ == "__main__":
     # --- MODELO 2D (Cuadrícula de 4x4 = 16 espines) ---
     calcular_energia_exacta(N=16, J=-1.0, dimensions=2, phase_name="Ferromagnética (FM)")
     calcular_energia_exacta(N=16, J= 1.0, dimensions=2, phase_name="Antiferromagnética (AFM)")
-    
